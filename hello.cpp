4c678a247fbcef3e995a73ab6034159448e53908
Von Reid <vontreid2000@gmail.com>
2020-01-14 16:51:16 -0700

    Von/Ronald first cs-1405 file

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// author: Von Reid, Ronald Hunt
+// started: 9 Jan 2020
+// purpose: print two greeting and compute a sum (basic c++ example program)
+
+// "iostream" declares input/output stuff like std::cout and std::endl
+#include <iostream>
+
+int main() {
+  std::cout << "Hello CS1405!" << std::endl;
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Sum of 2 through 8: " << (2+3+4+5+6+7+8) << std::endl;
+  return 0;
+}
