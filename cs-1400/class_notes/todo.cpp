470da86031dd7041472a2e417e0dd250cb95764a
Von Reid <vontreid2000@gmail.com>
2020-01-15 10:14:11 -0700

    todo is now stable lol

--- cs-1400/class_notes/todo.cpp
+++ cs-1400/class_notes/todo.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+
 // int is short for integer
 // this function will return the sum of a and b
 int plus(int a, int b) {
@@ -5,8 +10,20 @@ int plus(int a, int b) {
 }
 
 int main() {
-    // return an error code if something went wrong
-    // return 404;
+    // get todo items from user
+    string item1Name = "eat lunch";
+    bool item1Done = false;
+    float item1Importance = 2.5;
+    // minutes since the beginning of the year
+    int item1Due = 18 * 24 * 60; // jan 18th midnight
+
+    cout << "Name: " << item1Name << endl;
+    cout << "Importance: " << item1Importance << endl;
+    cout << "Due: " << item1Due << endl;
+    cout << "Done?: " << item1Done << endl;
+
+    // allow user to check off todo items
+
     // return 0 means "no errors to report"
     return 0;
 }
