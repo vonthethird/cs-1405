fe948076530b2923d68d73044004ee601a4e1bab
Von Reid <vontreid2000@gmail.com>
2020-01-15 09:59:58 -0700

    minor changes to dissection.cpp; added todo.cpp

---
+++ cs-1400/class_notes/todo.cpp
@@ -0,0 +1,12 @@
+// int is short for integer
+// this function will return the sum of a and b
+int plus(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    // return an error code if something went wrong
+    // return 404;
+    // return 0 means "no errors to report"
+    return 0;
+}
--- cs-1400/dissection.cpp
+++ cs-1400/dissection.cpp
@@ -1,6 +1,5 @@
 // A simple C++ program
 
-#include <iostream>
 /*
 # is a preprocessor directive, which reads the program before it is compiled. It sets up the source code for the compile.
 include is a directive, which causes the preprocessor to include the contents of a header file in the program.
@@ -8,20 +7,21 @@ iostream is the name of the header file.
 <> indicate that iostream is a standard C++ file.
 i.e. you cannot use "cout" without iostream
 */
-using namespace std;
+#include <iostream>
 /*
 this declares that the program will be accessing entities whose names are part of the namespace called "std".
 in order for a program to use the entities in iostream, it must have access to the std namespace.
 */
+using namespace std;
 
-int main()
 // this declares a function with the name "main"
 // everything inside of {} is the "main" function itself
+int main()
 {
-    cout << "Yeet skeet this program is pretty neat lol";
     // this will display everything inside of the string
     // a sring is "" wrapped around word(s)
-    return 0;
+    cout << "Yeet skeet this program is pretty neat lol";
     // after the cout successfuly prints out the string, this will send 0 to the operating system
     // the value 0 usually tells the OS that the program was executed successfully
+    return 0;
 }
