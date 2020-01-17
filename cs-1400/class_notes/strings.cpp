f41e4c56ca38c144312b6c56a7671c0f6cc98d2f
Von Reid <vontreid2000@gmail.com>
2020-01-17 10:08:20 -0700

    strings.cpp added to explain strings vs numbers

---
+++ cs-1400/class_notes/strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c = '3';
+    int n = 3;
+    int delta = 13;
+    char c2 = c + delta;
+    int n2 = n + delta;
+    cout << c << endl;
+    cout << n << endl;
+    cout << c2 << endl;
+    cout << n2 << endl;
+    return 0;
+}
