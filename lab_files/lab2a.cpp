1a8cf26ec1824ebb851881cccfb6c3f92d5abad2
Von Reid <vontreid2000@gmail.com>
2020-01-16 20:54:57 -0700

    lab files

---
+++ lab_files/lab2a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "    A" << endl;
+    cout << "   BCD" << endl;
+    cout << "  EFGHI" << endl;
+    cout << " JKLMNOP" << endl;
+    cout << "QRSTUVWXYZ" << endl << endl;
+
+    cout << "    A" << endl;
+    cout << "   BCD" << endl;
+    cout << "  EFGHI\n";
+    cout << " JKLMNOP" << endl;
+    cout << "QRSTUVWXYZ" << endl;
+    return 0;
+}
---
+++ lab_files/lab2b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int firstInt, secondInt;
+
+int main() {
+    // collect the user input
+    cout << "Enter first integer: ";
+    cin >> firstInt;
+    cout << "Enter second integer: ";
+    cin >> secondInt;
+    
+    // throw the inputs into some equations and spit them out
+    // sum will add them together
+    int sum = firstInt + secondInt;
+    cout << "Sum is............" << sum << endl;
+    // diff will subtract the second integer from the first
+    int diff = firstInt - secondInt;
+    cout << "Difference is....." << diff << endl;
+    // prod will multiply them
+    int prod = firstInt * secondInt;
+    cout << "Product is........" << prod << endl;
+    // quot will divide them
+    int quot = firstInt / secondInt;
+    cout << "Quotient is......." << quot << endl;
+    // remain will get the leftover remainder it would get from the division
+    int remain = firstInt % secondInt;
+    cout << "Remainder is......" << remain << endl;
+    
+    return 0;
+}
