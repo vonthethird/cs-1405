49233c2c6d58847818a59404faccc09197004ae5
Von Reid <vontreid2000@gmail.com>
2020-01-22 18:17:58 -0700

    started hwk #5 assignment

---
+++ cs-1400/hw5_chall3.cpp
@@ -0,0 +1,6 @@
+// Von Reid CS-1400, Chapter 2 Challenge 3 (HWK #5)
+// Write a program that will compute the total sales tax on a $95 purchase. Assume the state sales tax is 4 percent, and the county sales tax is 2 percent.
+#include <iostream>
+using namespace std;
+
+int subTotal = 95;
