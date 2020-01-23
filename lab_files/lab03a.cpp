014ca2d939205cd7a39edbcf01ee7a0b86760864
Von Reid <vontreid2000@gmail.com>
2020-01-23 08:31:47 -0700

    finished lab03a.cpp

--- lab_files/lab03a.cpp
+++ lab_files/lab03a.cpp
@@ -1,2 +1,18 @@
 // author: Von Reid, cs-1405 lab
 // this program will calculate the miles per gallon that a car gets
+#include <iostream>
+using namespace std;
+
+int gallons, miles;
+
+int main() {
+    // user greeting
+    cout << "I will calculate your car's mileage based off of the number gallons and miles traveled that you give me.\n";
+    cout << "Enter the number of gallons your car used on your last trip:\n";
+    cin >> gallons;
+    cout << "Enter the number of miles your car drove on your last trip:\n";
+    cin >> miles;
+    // miles per gallon just divides the gallons from the miles, which is literally the definition of it
+    int MpG = miles / gallons;
+    cout << "Your car gets " << MpG << " miles per gallon\n";
+}
--- lab_files/lab03b.cpp
+++ lab_files/lab03b.cpp
@@ -1,2 +1,4 @@
 // author: Von Reid, cs-1405 lab
 // this program will calculate the average of 5 test scores
+#include <iostream>
+using namespace std;
