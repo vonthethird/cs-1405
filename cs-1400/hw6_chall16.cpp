8d301e11f95052b26970ee79a7edbebc60e2fab0
Von Reid <vontreid2000@gmail.com>
2020-01-24 20:58:01 -0700

    cs-1400 hw#6 (chap2_chall16) finished

---
+++ cs-1400/hw6_chall16.cpp
@@ -0,0 +1,23 @@
+/* Von Reid CS-1400, Chapter 2 Challenge 16 (HWK #6)
+Write a program that will display a diamond pattern, like this:
+      *
+     ***
+    *****
+   *******
+    *****
+     ***
+      *
+[end of pattern] */
+
+#include <iostream>
+
+int main() {
+    std::cout << "      *\n";
+    std::cout << "     ***\n";
+    std::cout << "    *****\n";
+    std::cout << "   *******\n";
+    std::cout << "    *****\n";
+    std::cout << "     ***\n";
+    std::cout << "      *\n";
+    return 0;
+}
