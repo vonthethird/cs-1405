898feee301f32819371f39602c417024d4855b08
Von Reid <vontreid2000@gmail.com>
2020-01-29 21:15:07 -0700

    hw8 stuff lolol

---
+++ cs-1400/hw8_stuff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// problem 4
+float a, b, c;
+b = a + 2;
+a = b * 4;
+b = a / 3.14;
+a = b - 8;
+a = 27;
+c = 'K';
+c = int('B');
+// problem 27
+int speed = 20;
+int time = 10;
+int distance = speed * time;
+cout << distance;
+return 0;
+}
