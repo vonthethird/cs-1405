b991b880993f52c427d3c9834b2c68cae0d8950d
Von Reid <vontreid2000@gmail.com>
2020-01-30 22:59:06 -0700

    hw8 stuff

---
+++ cs-1400/hw8_stuff.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// problem 4
+float a, b, c;
+b = a + 2;
+a = b * 4;
+b = a / 3.14;
+a = b - 8;
+a = 27;
+c = 'K';
+c = int('B');
+// problem 27
+int speed = 20;
+int time = 10;
+int distance = speed * time;
+cout << distance;
+// problem 28
+float force = 172;
+float area = 27.5;
+float pressure = area / force;
+cout << pressure;
+// return boi
+return 0;
+}
