59f7df22ca532ce3b31f3d3b690e1b6b32da988c
Von Reid <vontreid2000@gmail.com>
2020-01-31 10:04:18 -0700

    started sticks.cpp lab

---
+++ lab_files/sticks.cpp
@@ -0,0 +1,18 @@
+// author: Von Reid, Garrett Record
+// does a thing
+#include <iostream>
+// I don't care about the std:: stuff--this is way easier
+using namespace std;
+
+int main () {
+    int sticksTaken, numSticksLeft, turnsTaken, currentPlayer;
+    currentPlayer = (turnsTaken % 2) + 1;
+    while (numSticksLeft > 0) {
+        numSticksLeft = 20;
+        cout << currentPlayer << ", how many sticks would you like to take?\n";
+        cin >> sticksTaken;
+        numSticksLeft = numSticksLeft - sticksTaken;
+        cout << "Sticks left: " << numSticksLeft << endl;
+    }
+    return 0;
+}
