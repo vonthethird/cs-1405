d41fa88a91eb8fb395c368915fe4cbdd969241ae
Von Reid <vontreid2000@gmail.com>
2020-01-31 15:13:06 -0700

    finished sticks.cpp but still need to do extra credit

--- lab_files/sticks.cpp
+++ lab_files/sticks.cpp
@@ -4,22 +4,33 @@
 // I don't care about the std:: stuff--this is way easier
 using namespace std;
 
-int main () {
+int main()
+{
     int sticksTaken, numSticksLeft = 20, turnsTaken = 0, currentPlayer;
 
-    while (numSticksLeft > 0) {
-    /*
-    if turnsTaken is odd, currentPlayer is an even number
-    if turnsTaken is even, currentPlayer is an odd number
-    i.e. if turnsTaken == 3, currentPlayer == 2
-    */
+    while (numSticksLeft > 0)
+    {
+        /*
+        if turnsTaken is odd, currentPlayer is an even number
+        if turnsTaken is even, currentPlayer is an odd number
+        i.e. if turnsTaken == 3, currentPlayer == 2
+        */
         currentPlayer = (turnsTaken % 2) + 1;
-        cout << "Player" << currentPlayer << ", how many sticks would you like to take?\n";
+        cout << "Player " << currentPlayer << ", how many sticks would you like to take?\n";
         cin >> sticksTaken;
         numSticksLeft = numSticksLeft - sticksTaken;
-        cout << "Sticks left: " << numSticksLeft << endl;
-        turnsTaken++;
-        cout << "turn: " << turnsTaken << endl;
+
+        if (numSticksLeft > 0)
+        {
+            cout << "Sticks left: " << numSticksLeft << endl;
         }
+        // increment the number of turns taken so currentPlayer can have a correct value
+        turnsTaken++;
+    }
+
+    // the last person to take sticks is the loser, so the winner's Player number is the opposite of currentPlayer
+    int winner = currentPlayer % 2 + 1;
+    cout << "Player " << winner << " won!\n";
+
     return 0;
 }
