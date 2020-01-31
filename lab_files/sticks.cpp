f8fe5c3231f0bc9993cf71d6422af2c65cf085cd
Von Reid <vontreid2000@gmail.com>
2020-01-31 13:43:03 -0700

    added some unfinished pseudocode with sticks.pseudo

--- lab_files/sticks.cpp
+++ lab_files/sticks.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 int main () {
     int sticksTaken, numSticksLeft, turnsTaken = 0, currentPlayer;
-    if (turnsTaken )
+    //if (turnsTaken )
     /*
     if turnsTaken is odd, currentPlayer is an even number
     if turnsTaken is even, currentPlayer is an odd number
     i.e. if turnsTaken == 3, currentPlayer == 2
     */
     currentPlayer = (turnsTaken % 2) + 1;
-    while (numSticksLeft > 0) {
-        numSticksLeft = 20;
-        cout << currentPlayer << ", how many sticks would you like to take?\n";
-        cin >> sticksTaken;
-        numSticksLeft = numSticksLeft - sticksTaken;
-        cout << "Sticks left: " << numSticksLeft << endl;
+    for (turnsTaken ) {
+        while (numSticksLeft > 0) {
+            numSticksLeft = 20;
+            cout << currentPlayer << ", how many sticks would you like to take?\n";
+            cin >> sticksTaken;
+            numSticksLeft = numSticksLeft - sticksTaken;
+            cout << "Sticks left: " << numSticksLeft << endl;
+        }
     }
     return 0;
 }
