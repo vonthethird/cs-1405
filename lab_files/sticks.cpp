35f33cd71b97b7f756c7f2d0403b621077850c79
Von Reid <vontreid2000@gmail.com>
2020-01-31 14:46:18 -0700

    almost done with sticks

--- lab_files/sticks.cpp
+++ lab_files/sticks.cpp
@@ -5,22 +5,21 @@
 using namespace std;
 
 int main () {
-    int sticksTaken, numSticksLeft, turnsTaken = 0, currentPlayer;
-    //if (turnsTaken )
+    int sticksTaken, numSticksLeft = 20, turnsTaken = 0, currentPlayer;
+
+    while (numSticksLeft > 0) {
     /*
     if turnsTaken is odd, currentPlayer is an even number
     if turnsTaken is even, currentPlayer is an odd number
     i.e. if turnsTaken == 3, currentPlayer == 2
     */
-    currentPlayer = (turnsTaken % 2) + 1;
-    for (turnsTaken ) {
-        while (numSticksLeft > 0) {
-            numSticksLeft = 20;
-            cout << currentPlayer << ", how many sticks would you like to take?\n";
-            cin >> sticksTaken;
-            numSticksLeft = numSticksLeft - sticksTaken;
-            cout << "Sticks left: " << numSticksLeft << endl;
+        currentPlayer = (turnsTaken % 2) + 1;
+        cout << "Player" << currentPlayer << ", how many sticks would you like to take?\n";
+        cin >> sticksTaken;
+        numSticksLeft = numSticksLeft - sticksTaken;
+        cout << "Sticks left: " << numSticksLeft << endl;
+        turnsTaken++;
+        cout << "turn: " << turnsTaken << endl;
         }
-    }
     return 0;
 }
