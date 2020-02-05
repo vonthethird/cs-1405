4139db93fac1554bf5ea08f0bc62433651168407
Von Reid <vontreid2000@gmail.com>
2020-02-05 09:52:14 -0700

    added rooms.cpp from cs1400 class notes

---
+++ cs-1400/class_notes/rooms.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+
+int main() {
+    double a1 = 503.2377234278949;
+    double b1 = 234.34;
+    double a2 = 2.23;
+    double b2 = 123.3;
+    cout << setw(10) << "a" << " " << setw(6) << "b" << endl;
+    cout << setw(10) << setprecision(20) << a1 << " " << setw(3) << b1 << endl;
+    cout << setw(10) << setprecision(3) << a2 << " " << setw(3) << b2 << endl;
+    return 0;
+}
