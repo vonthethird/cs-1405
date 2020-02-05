a41a1e6aa9aaa0a1ec94cd5c6e6487fea6f77e4e
Von Reid <vontreid2000@gmail.com>
2020-02-05 10:19:52 -0700

    started to actually make the minigame in rooms.cpp

--- cs-1400/class_notes/rooms.cpp
+++ cs-1400/class_notes/rooms.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <iomanip>
 
+using std::cin;
 using std::cout;
 using std::endl;
-using std::setw;
 using std::setprecision;
+using std::setw;
 
+/* We did this in class to demo setw() and setprecision() so I commented it out to use as an example in case we need it as a reference.
 int main() {
     double a1 = 503.2377234278949;
     double b1 = 234.34;
@@ -15,4 +17,26 @@ int main() {
     cout << setw(10) << setprecision(20) << a1 << " " << setw(3) << b1 << endl;
     cout << setw(10) << setprecision(3) << a2 << " " << setw(3) << b2 << endl;
     return 0;
+}
+*/
+
+int promptQuestions()
+{
+    char promptAnswer;
+    cout << "From the 3 options, enter 1 letter:\na, b, or c\n";
+    cout << "a) Move North\n"
+         << "b) Move East\n"
+         << "c) Go to sleep\n";
+    cin >> promptAnswer;
+    while ((promptAnswer != 'a') && (promptAnswer != 'b') && (promptAnswer != 'c'))
+    {
+        cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
+        cin >> promptAnswer;
+    }
+}
+
+int main()
+{
+    cout << "Hello, welcome to the game!\nYou may or may not survive this, so be careful.\n\n";
+    promptQuestions();
 }
