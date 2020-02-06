75fabcfb5d2fe9e85d54d3f2ddbf12f65efcf3cf
Von Reid <vontreid2000@gmail.com>
2020-02-05 17:30:26 -0700

    got rooms.cpp to work with the 3 abc options

--- extra_projects/rooms.cpp
+++ extra_projects/rooms.cpp
@@ -1,13 +1,70 @@
+/*
+author: Von Reid
+This is a game that involves moving directions in a place with multiple rooms.
+My in-class group got the Southwest room, which is why you can only move North and East.
+*/
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <limits>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+/*int pressEnter()
+{
+    cout << "Press the ENTER key";
+    if (cin.std::istream::get() == '\n')
+        cout << "Good job.\n";
+    else
+        cout << "I meant ONLY the ENTER key... Oh well.\n";
+    return 0;
+}*/
+
+int waitBoi()
+{
+    std::chrono::seconds dura(1);
+    std::this_thread::sleep_for(dura);
+}
+
+int optionC()
+{
+    cin.clear();
+    float hoursSlept = 0;
+    cout << "\nHow many hours do you want to sleep?\n";
+    cin >> hoursSlept;
+    /* tried to prevent user from entering anything that's not a number, can't figure it out. I'll come back to it later.
+    while (cin.fail())
+    {
+        cin.clear();
+        cout << "Dude. That's not a number. I expected better than that from you.";
+        cin >> hoursSlept;
+    }
+    */
+    waitBoi();
+    cout << ".\n";
+    waitBoi();
+    cout << "...\n";
+    waitBoi();
+    cout << ".....\n";
+    waitBoi();
+    cout << ".......\n";
+    waitBoi();
+    if (hoursSlept == 1)
+    {
+        cout << "You slept for " << hoursSlept << " hour.\nGood job.\n\n";
+    }
+    else
+    {
+        cout << "You slept for " << hoursSlept << " hours.\nGood job.\n\n";
+    }
+}
+
 int promptQuestions()
 {
     char promptAnswer;
-    cout << "From the 3 options, enter 1 letter:\na, b, or c\n";
+    cout << "From the 3 options, enter 1 letter (a, b, or c):\n";
     cout << "a) Move North\n"
          << "b) Move East\n"
          << "c) Go to sleep\n";
@@ -16,6 +73,19 @@ int promptQuestions()
     {
         cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
         cin >> promptAnswer;
+    }
+    if (promptAnswer == 'a')
+    {
+        cout << "\nYou moved North.\n";
+    }
+    else if (promptAnswer == 'b')
+    {
+        cout << "\nYou moved East.\n";
+    }
+    else if (promptAnswer == 'c')
+    {
+        optionC();
+        promptQuestions();
     }
 }
 
