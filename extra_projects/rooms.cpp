920c4c2919ac49f25fc899d7a87ab8502d10af91
Von Reid <vontreid2000@gmail.com>
2020-02-05 18:19:48 -0700

    kind of a mess, but rooms.cpp is really coming together.

--- extra_projects/rooms.cpp
+++ extra_projects/rooms.cpp
@@ -10,7 +10,6 @@ My in-class group got the Southwest room, which is why you can only move North a
 
 using std::cin;
 using std::cout;
-using std::endl;
 
 /*int pressEnter()
 {
@@ -21,6 +20,9 @@ using std::endl;
         cout << "I meant ONLY the ENTER key... Oh well.\n";
     return 0;
 }*/
+
+char promptAnswer;
+int roomNumber;
 
 int waitBoi()
 {
@@ -59,12 +61,42 @@ int optionC()
     {
         cout << "You slept for " << hoursSlept << " hours.\nGood job.\n\n";
     }
+    return 0;
 }
 
-int promptQuestions()
+int roomOneQuestions()
 {
-    char promptAnswer;
-    cout << "From the 3 options, enter 1 letter (a, b, or c):\n";
+    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
+    cout << "a) Move South\n"
+         << "b) Move East\n"
+         << "c) Go to sleep\n";
+    cin >> promptAnswer;
+    while ((promptAnswer != 'a') && (promptAnswer != 'b') && (promptAnswer != 'c'))
+    {
+        cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
+        cin >> promptAnswer;
+    }
+    /*if (promptAnswer == 'a')
+    {
+        cout << "\nYou moved South.\n";
+        cout << "\nYou are now in room 2.\n\n";
+        //roomTwoQuestions();
+    }
+    else if (promptAnswer == 'b')
+    {
+        cout << "\nYou moved East.\n";
+    }
+    else if (promptAnswer == 'c')
+    {
+        optionC();
+        roomOneQuestions();
+    }*/
+    return 0;
+}
+
+int roomTwoQuestions()
+{
+    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
     cout << "a) Move North\n"
          << "b) Move East\n"
          << "c) Go to sleep\n";
@@ -74,7 +106,7 @@ int promptQuestions()
         cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
         cin >> promptAnswer;
     }
-    if (promptAnswer == 'a')
+    /*if (promptAnswer == 'a')
     {
         cout << "\nYou moved North.\n";
     }
@@ -85,12 +117,70 @@ int promptQuestions()
     else if (promptAnswer == 'c')
     {
         optionC();
-        promptQuestions();
+        roomTwoQuestions();
+    }*/
+    return 0;
+}
+
+int pickRoom()
+{
+    cout << "There are 4 numbered rooms.\nPick a room number to start in. (1, 2, 3, or 4)\n";
+    cin >> roomNumber;
+    while ((roomNumber != 1) && (roomNumber != 2) && (roomNumber != 3) && (roomNumber != 4))
+    {
+        cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
+        cin >> roomNumber;
+    }
+    switch (roomNumber)
+    {
+    case 1:
+        cout << "\nYou are now in room 1.\n\n";
+        break;
+    case 2:
+        cout << "\nYou are now in room 2.\n\n";
+        roomTwoQuestions();
+        break;
+    case 3:
+        cout << "\nYou are now in room 3.\n\n";
+        break;
+    case 4:
+        cout << "\nYou are now in room 4.\n\n";
+        break;
+    }
+    return 0;
+}
+
+int roomOneChoices()
+{
+    if (promptAnswer == 'a')
+    {
+        cout << "\nYou moved South.\n";
+        cout << "\nYou are now in room 2.\n\n";
+        roomNumber == 2;
+        roomTwoQuestions();
+    }
+    else if (promptAnswer == 'b')
+    {
+        cout << "\nYou moved East.\n";
+    }
+    else if (promptAnswer == 'c')
+    {
+        optionC();
+        roomOneQuestions();
     }
 }
 
 int main()
 {
     cout << "Hello, welcome to the game!\nYou may or may not survive this, so be careful.\n\n";
-    promptQuestions();
+    pickRoom();
+    while (roomNumber < 5)
+    {
+        while (roomNumber == 1)
+        {
+            roomOneQuestions();
+            roomOneChoices();
+        }
+    }
+    return 0;
 }
