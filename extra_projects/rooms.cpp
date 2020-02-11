1698eb4076275742b95e53b26614995f85db64e1
Von Reid <vontreid2000@gmail.com>
2020-02-11 13:23:19 -0700

    more to rooms

--- extra_projects/rooms.cpp
+++ extra_projects/rooms.cpp
@@ -6,18 +6,78 @@ My in-class group got the Southwest room, which is why you can only move North a
 #include <iostream>
 #include <limits>
 
+#include <vector> // for the vector image in zeldaASCII
+
 #include <chrono> // <chrono> and <thread> are required for the time-pausing stuff in waitBoi()
 #include <thread>
 
 using std::cin;
 using std::cout;
+using std::endl;
 
 char promptAnswer;
 int roomNumber, sleepCounter = 0, travelCounter = 0;
+void print(const std::vector<std::string> &picture)
+{
+    std::cout << '\n';
+    for (const auto &line : picture)
+        std::cout << line << '\n';
+    std::cout << '\n';
+}
+std::vector<std::string> zeldaASCII =
+    {
+        R"(                                    /@)",
+        R"(                     __        __   /\/)",
+        R"(                    /==\      /  \_/\/)",
+        R"(                  /======\    \/\__ \__)",
+        R"(                /==/\  /\==\    /\_|__ \)",
+        R"(             /==/    ||    \=\ / / / /_/)",
+        R"(           /=/    /\ || /\   \=\/ /)",
+        R"(        /===/   /   \||/   \   \===\)",
+        R"(      /===/   /_________________ \===\)",
+        R"(   /====/   / |                /  \====\)",
+        R"( /====/   /   |  _________    /  \   \===\    THE LEGEND OF)",
+        R"( /==/   /     | /   /  \ / / /  __________\)",
+        R"(|===| /       |/   /____/ / /   \   _____ |\   /      \   _ \      \  \)",
+        R"( \==\             /\   / / /     | |  /= \| | |        | | \ \     / _ \)",
+        R"( \===\__    \    /  \ / / /   /  | | /===/  | |        | |  \ \   / / \ \)",
+        R"(   \==\ \    \\ /____/   /_\ //  | |_____/| | |        | |   | | / /___\ \)",
+        R"(   \===\ \   \\\\\\\/   /////// /|  _____ | | |        | |   | | |  ___  |)",
+        R"(     \==\/     \\\\/ / //////   \| |/==/ \| | |        | |   | | | /   \ |)",
+        R"(     \==\     _ \\/ / /////    _ | |==/     | |        | |  / /  | |   | |)",
+        R"(       \==\  / \ / / ///      /|\| |_____/| | |_____/| | |_/ /   | |   | |)",
+        R"(       \==\ /   / / /________/ |/_________|/_________|/_____/   /___\ /___\)",
+        R"(         \==\  /               | /==/)",
+        R"(         \=\  /________________|/=/)",
+        R"(           \==\     _____     /==/)",
+        R"(          / \===\   \   /   /===/)",
+        R"(         / / /\===\  \_/  /===/)",
+        R"(        / / /   \====\ /====/)",
+        R"(       / / /      \===|===/)",
+        R"(       |/_/         \===/)",
+        R"(                      =)"};
+
+int pickRoom()
+{
+    cout << "There are 4 numbered rooms." << endl
+         << "Pick a room number to start in. (1, 2, 3, or 4)" << endl;
+    cin >> roomNumber;
+    while ((roomNumber != 1) && (roomNumber != 2) && (roomNumber != 3) && (roomNumber != 4))
+    {
+        cout << endl
+             << "You dumb, stupid, incompetent idiot." << endl
+             << "That's not a valid option. Try again." << endl;
+        cin >> roomNumber;
+    }
+    cout << endl;
+    return 0;
+}
 
 int changeRoom()
 {
-    cout << "\nYou are now in room " << roomNumber << ".\n\n";
+    cout << endl
+         << "You are now in room " << roomNumber << "." << endl
+         << endl;
     return 0;
 }
 
@@ -25,7 +85,9 @@ int invalidPromptCheck()
 {
     while ((promptAnswer != 'a') && (promptAnswer != 'b') && (promptAnswer != 'c'))
     {
-        cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
+        cout << endl
+             << "You dumb, stupid, incompetent idiot." << endl
+             << "That's not a valid option. Try again." << endl;
         cin >> promptAnswer;
     }
 }
@@ -40,7 +102,8 @@ int optionC()
 {
     cin.clear();
     float hoursSlept = 0;
-    cout << "\nHow many hours do you want to sleep?\n";
+    cout << endl
+         << "How many hours do you want to sleep?" << endl;
     cin >> hoursSlept;
     /* tried to prevent user from entering anything that's not a number, can't figure it out. I'll come back to it later.
     while (cin.fail())
@@ -51,42 +114,132 @@ int optionC()
     }
     */
     waitBoi();
-    cout << ".\n";
+    cout << "." << endl;
     waitBoi();
-    cout << "...\n";
+    cout << "..." << endl;
     waitBoi();
-    cout << ".....\n";
+    cout << "....." << endl;
     waitBoi();
-    cout << ".......\n";
+    cout << "......." << endl;
     waitBoi();
+
+    // Changes plural "hours" to singular "hour" for the possibility of 1 hour slept
     if (hoursSlept == 1)
     {
-        cout << "You slept for " << hoursSlept << " hour.\nGood job.\n\n";
+        cout << "You slept for " << hoursSlept << " hour." << endl
+             << "Good job." << endl
+             << endl;
     }
     else
     {
-        cout << "You slept for " << hoursSlept << " hours.\nGood job.\n\n";
+        cout << "You slept for " << hoursSlept << " hours." << endl
+             << "Good job." << endl
+             << endl;
     }
+
     return 0;
+}
+
+int altPrompts()
+{
+    std::string optionThree, optionThreeResult;
+    if (roomNumber == 2)
+    {
+        optionThree = "3)Crawl into the deep hole in the ground";
+    }
+    else if (roomNumber == 3)
+    {
+        optionThree = "3)Approach the dark figure moving in the corner of the room";
+        optionThreeResult = "";
+    }
+    cout << "What will you do now?" << endl;
+    int altPromptAnswer;
+    cout << "1)Pick up mysterious glowing mushroom on the wall" << endl
+         << "2)Give up" << endl
+         << optionThree << endl
+         << "4)Go back to the previous choices for this room" << endl;
+    cin >> altPromptAnswer;
+    switch (altPromptAnswer)
+    {
+    case 1:
+        waitBoi();
+        cout << "." << endl;
+        waitBoi();
+        cout << "..." << endl;
+        cout << endl
+             << "After a few minutes, all of the colors around you start to flow together." << endl
+             << "You see a whole new world of colors. An especially bright red light shines through a portal in the wall." << endl
+             << "Will you jump into the portal?" << endl
+             << "<y>/<n>" << endl;
+        char portalAnswer;
+        cin >> portalAnswer;
+        cout << endl
+             << "It doesn't matter what you chose, because you have no control anymore." << endl
+             << "You jump into the portal head-first." << endl;
+
+        break;
+    case 2:
+        cout << "You never escaped the rooms..." << endl
+             << "Oh well..." << endl
+             << "Goodnight." << endl;
+        break;
+    case 3:
+        if (roomNumber == 2)
+        {
+            cout << "You crawl for a little while." << endl;
+            waitBoi();
+            cout << "." << endl;
+            waitBoi();
+            cout << "..." << endl;
+            waitBoi();
+            cout << "....." << endl;
+            waitBoi();
+            cout << "You appear in a foggy room. You see an old man with a white beard, which hasn't been trimmed in centuries." << endl;
+            cout << "The old man looks you in the eyes and yells, 'It's dangerous to go alone! Take this!'" << endl
+                 << "He places a beautiful sword in your hands. 'You are the chosen one, the hero of courage! You must save us!'" << endl;
+            cout << "You nod, then exit the cave. You're no longer in the random room labyrinth, you're now in Hyrule." << endl
+                 << "The people of this world need you, so you're now their savior. Defeat the evil Lord Ganon and restore the Triforce to its former glory." << endl
+                 << endl;
+            waitBoi();
+            cout << "Only you can save us, Link." << endl;
+            waitBoi();
+            print(zeldaASCII);
+            waitBoi();
+            char anyLetterToContinue;
+            cout << endl
+                 << "Please enter any letter to continue." << endl;
+            cin >> anyLetterToContinue;
+        }
+        else if (roomNumber == 3)
+        {
+            cout << "";
+        }
+        break;
+    case 4:
+        break;
+    }
 }
 
 int roomOne()
 {
-    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
-    cout << "a) Move South\n"
-         << "b) Move East\n"
-         << "c) Go to sleep\n";
+    cout << "There are 3 choices you can make." << endl
+         << "Enter 1 letter (a, b, or c) to choose:" << endl;
+    cout << "a) Move South" << endl
+         << "b) Move East" << endl
+         << "c) Go to sleep" << endl;
     cin >> promptAnswer;
     invalidPromptCheck();
     switch (promptAnswer)
     {
     case 'a':
-        cout << "\nYou moved South.\n";
+        cout << endl
+             << "You moved South." << endl;
         roomNumber = 2, travelCounter++;
         changeRoom();
         break;
     case 'b':
-        cout << "\nYou moved East.\n";
+        cout << endl
+             << "You moved East." << endl;
         roomNumber = 4, travelCounter++;
         changeRoom();
         break;
@@ -100,77 +253,90 @@ int roomOne()
 
 int roomTwo()
 {
-    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
-    cout << "a) Move North\n"
-         << "b) Move East\n"
-         << "c) Go to sleep\n";
+    cout << "There are 3 choices you can make." << endl
+         << "Enter 1 letter (a, b, or c) to choose:" << endl;
+    cout << "a) Move North" << endl
+         << "b) Move East" << endl
+         << "c) Go to sleep" << endl;
     cin >> promptAnswer;
     invalidPromptCheck();
     switch (promptAnswer)
     {
     case 'a':
-        cout << "\nYou moved North.\n";
+        cout << endl
+             << "You moved North." << endl;
         roomNumber = 1, travelCounter++;
         changeRoom();
         break;
     case 'b':
-        cout << "\nYou tried moving East, but there turns out to be no door.\n";
-        cout << "What will you do now?\n";
+        cout << endl
+             << "You tried moving East, but there turns out to be no door." << endl
+             << endl;
         roomNumber = 2;
+        altPrompts();
         break;
     case 'c':
         optionC();
         roomNumber = 2, sleepCounter++;
         break;
     }
+
     return 0;
 }
 
 int roomThree()
 {
-    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
-    cout << "a) Move North\n"
-         << "b) Move West\n"
-         << "c) Go to sleep\n";
+    cout << "There are 3 choices you can make." << endl
+         << "Enter 1 letter (a, b, or c) to choose:" << endl;
+    cout << "a) Move North" << endl
+         << "b) Move West" << endl
+         << "c) Go to sleep" << endl;
     cin >> promptAnswer;
     invalidPromptCheck();
     switch (promptAnswer)
     {
     case 'a':
-        cout << "\nYou moved North.\n";
+        cout << endl
+             << "You moved North." << endl;
         roomNumber = 4, travelCounter++;
         changeRoom();
         break;
     case 'b':
-        cout << "\nYou tried moving West, but there turns out to be no door.\n";
-        cout << "What will you do now?\n";
+        cout << endl
+             << "You tried moving West, but there turns out to be no door." << endl
+             << endl;
         roomNumber = 3;
+        altPrompts();
         break;
     case 'c':
         optionC();
         roomNumber = 3, sleepCounter++;
         break;
     }
+
     return 0;
 }
 
 int roomFour()
 {
-    cout << "There are 3 choices you can make.\nEnter 1 letter (a, b, or c) to choose:\n";
-    cout << "a) Move South\n"
-         << "b) Move West\n"
-         << "c) Go to sleep\n";
+    cout << "There are 3 choices you can make." << endl
+         << "Enter 1 letter (a, b, or c) to choose:" << endl;
+    cout << "a) Move South" << endl
+         << "b) Move West" << endl
+         << "c) Go to sleep" << endl;
     cin >> promptAnswer;
     invalidPromptCheck();
     switch (promptAnswer)
     {
     case 'a':
-        cout << "\nYou moved South.\n";
+        cout << endl
+             << "You moved South." << endl;
         roomNumber = 3, travelCounter++;
         changeRoom();
         break;
     case 'b':
-        cout << "\nYou moved West.\n";
+        cout << endl
+             << "You moved West." << endl;
         roomNumber = 1, travelCounter++;
         changeRoom();
         break;
@@ -179,24 +345,15 @@ int roomFour()
         roomNumber = 4, sleepCounter++;
         break;
     }
-    return 0;
-}
 
-int pickRoom()
-{
-    cout << "There are 4 numbered rooms.\nPick a room number to start in. (1, 2, 3, or 4)\n";
-    cin >> roomNumber;
-    while ((roomNumber != 1) && (roomNumber != 2) && (roomNumber != 3) && (roomNumber != 4))
-    {
-        cout << "\nYou dumb, stupid, incompetent idiot.\nThat's not a valid option. Try again.\n";
-        cin >> roomNumber;
-    }
     return 0;
 }
 
 int main()
 {
-    cout << "Hello, welcome to the game!\nYou may or may not survive this, so be careful.\n\n";
+    cout << "Hello, welcome to the game!" << endl
+         << "You may or may not survive this, so be careful." << endl
+         << endl;
     pickRoom();
     cin.clear();
     while (roomNumber < 5)
@@ -221,7 +378,18 @@ int main()
             roomNumber = 5;
         }
     }
-    cout << "\n\n***Suddenly, you woke up and realized it was all a dream.***\nThere are no rooms, and there are no doors.\nThis was simply a figment of your imagination.\nBut hey, at least you finally escaped! (for now.....)\n\n";
-    cout << "You slept " << sleepCounter << " times, and you went to a different room " << travelCounter << " times.\nYou were quite determined to escape. I'll have to find better ways to trick you next time...\n\n";
+    if (roomNumber == 5)
+    {
+        cout << endl
+             << endl
+             << "***Suddenly, you woke up and realized it was all a dream.***" << endl
+             << "There are no rooms, and there are no doors." << endl
+             << "This was simply a figment of your imagination." << endl
+             << "But hey, at least you finally escaped! (for now.....)" << endl
+             << endl;
+        cout << "You slept " << sleepCounter << " times, and you went to a different room " << travelCounter << " times." << endl
+             << "You were quite determined to escape. I'll have to find better ways to trick you next time..." << endl
+             << endl;
+    }
     return 0;
 }
