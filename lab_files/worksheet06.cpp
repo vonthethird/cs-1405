c7293f175de2b4eb06068d99945bee7a374299f6
Von Reid <vontreid2000@gmail.com>
2020-02-11 20:33:30 -0700

    set up the rest of the damn worksheet

---
+++ lab_files/worksheet06.cpp
@@ -0,0 +1,58 @@
+// author: Von Reid
+// CS-1405 Worksheet #6, due 02-14-2020
+// Look at my comments to guide you through the code for the assignment's problems
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// 1.
+int firstProblem()
+{
+    // subproblem 1.
+    for (int i = -10; i <= 10; i++)
+    {
+        // cout << i << endl;
+    }
+    // subproblem 2.
+    for (int i = 100; i >= 1; i--)
+    {
+        // cout << i << endl;
+    }
+}
+
+// 2.
+int secondProblem(){
+
+}
+
+// 3.
+int thirdProblem(){
+
+}
+
+// 4.
+int fourthProblem(){
+
+}
+
+// 5.
+int fifthProblem(){
+
+}
+
+// 6.
+int sixthProblem(){
+
+}
+
+// executes all 6 problems
+int main()
+{
+    firstProblem();
+    secondProblem();
+    thirdProblem();
+    fourthProblem();
+    fifthProblem();
+    sixthProblem();
+}
