2ade1ecbd9452e0b94b8f424c31503a215012206
Von Reid <vontreid2000@gmail.com>
2020-02-12 16:40:28 -0700

    here's what I've started with hw15

---
+++ cs-1400/hw_assignments/hw15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// author: Von Reid
+// this is for the H.W. #15. assignment in class
+
+int main()
+{
+
+int i;
+/*  5.
+&& makes it so the statement returns true if both statements are true.
+i.e.    */
+
+if (i < 10 && i > 0){} // will only process if i is both greater than 0 and less than 10
+
+
+/*  6.
+|| makes it so the statement returns true if one of the statements is true.
+i.e.    */
+if ((i < 10) || (i > 0)){}  // will only process if i is either greater than 0 or less than 10, so only one of them has to be true.
+
+}
