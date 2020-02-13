f22afbee1be99f3bf2f00d6aa6f62a67f240b8d6
Von Reid <vontreid2000@gmail.com>
2020-02-13 15:43:33 -0700

    started lab06

---
+++ lab_files/lab06.cpp
@@ -0,0 +1,15 @@
+// author: Von Reid
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int fibNum;
+    cout << "Enter a number to continue the fibonnaci sequence.";
+    cin >> fibNum;
+
+    return 0;
+}
