d4dc5cdd712688de3680d57e082ce18aab7ac53d
Von Reid <vontreid2000@gmail.com>
2020-02-12 23:08:32 -0700

    hw15 update

--- cs-1400/hw_assignments/hw15.cpp
+++ cs-1400/hw_assignments/hw15.cpp
@@ -18,4 +18,20 @@ if (i < 10 && i > 0){} // will only process if i is both greater than 0 and less
 i.e.    */
 if ((i < 10) || (i > 0)){}  // will only process if i is either greater than 0 or less than 10, so only one of them has to be true.
 
+
+//  18. &&
+//  19. ||
+//  20. !
+//  21. left-to-right
+//  22. &&
+//  23. ||
+//  34. 
+        bool minimum = false;
+        int hours = 0;
+        if (minimum)
+        {
+            hours = 10;
+        }
+
+
 }
