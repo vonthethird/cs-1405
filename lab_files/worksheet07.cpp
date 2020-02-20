c6166cbe359ec1125740d2b8420e7bd664ad1f04
Von Reid <vontreid2000@gmail.com>
2020-02-19 19:20:45 -0700

    started worksheet07

---
+++ lab_files/worksheet07.cpp
@@ -0,0 +1,11 @@
+// author: Von Reid
+// CS-1405 Worksheet #7, due 02-20-2020
+// Look at my comments to guide you through the code for the assignment's problems
+
+#include <iostream>
+
+// 1. A parameter is a special variable that holds a value being passed into a function
+
+int main()
+{
+}
