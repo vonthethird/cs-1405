137b1aeceb2a1eff769d5441fab1024ada0b7aa4
Von Reid <vontreid2000@gmail.com>
2020-02-21 22:51:36 -0700

    shortened some unit names to match the other shortened unit names

--- lab_files/lab07.cpp
+++ lab_files/lab07.cpp
@@ -10,9 +10,9 @@ using std::endl;
 using std::string;
 
 int isClose(float, float);
-int inchToCm(float);
-int yardToM(float);
-int mileToKm(float);
+int inToCm(float);
+int ydToM(float);
+int miToKm(float);
 void testDriver();
 int floatInp(string);
 float floatOutp(float, string);
@@ -26,15 +26,15 @@ int main()
     testDriver();
 
     floatInp("Please enter how many inches you would like to convert into centimeters.");
-    inchToCm(usrFloat);
+    inToCm(usrFloat);
     floatOutp(cm, "cm");
 
     floatInp("Please enter how many yards you would like to convert into meters.");
-    yardToM(usrFloat);
+    ydToM(usrFloat);
     floatOutp(m, "m");
 
     floatInp("Please enter how many miles you would like to convert into kilometers.");
-    mileToKm(usrFloat);
+    miToKm(usrFloat);
     floatOutp(km, "km");
 
     return 0;
@@ -66,24 +66,24 @@ int isClose(float num1, float num2)
     }
 }
 
-int inchToCm(float inch)
+int inToCm(float in)
 {
     float const cmConversion = 2.54;
-    cm = inch * cmConversion;
+    cm = in * cmConversion;
     return cm;
 }
 
-int yardToM(float yard)
+int ydToM(float yd)
 {
     float const mConversion = 0.9144;
-    m = yard * mConversion;
+    m = yd * mConversion;
     return m;
 }
 
-int mileToKm(float mile)
+int miToKm(float mi)
 {
     float const kmConversion = 1.609344;
-    km = mile * kmConversion;
+    km = mi * kmConversion;
     return km;
 }
 
@@ -94,7 +94,7 @@ void testDriver()
     // assert(isClose(40000, 5));
 
     /* honestly, I have no idea how to do 3b, I started to do this but it's not working like it's supposed to.
-    float cm1 = inchToCm(close1), cm2 = inchToCm(close2), m1 = yardToM(close1), m2 = yardToM(close2), km1 = mileToKm(close1), km2 = mileToKm(close2);
+    float cm1 = inToCm(close1), cm2 = inToCm(close2), m1 = ydToM(close1), m2 = ydToM(close2), km1 = miToKm(close1), km2 = miToKm(close2);
     assert(isClose(cm1, cm2));
     assert(isClose(m1, m2));
     assert(isClose(km1, km2)); */
