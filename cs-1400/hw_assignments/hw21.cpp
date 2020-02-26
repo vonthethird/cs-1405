14ab86c54584a5200d41746bb24ea99e7189f055
Von Reid <vontreid2000@gmail.com>
2020-02-26 15:21:27 -0700

    finished hw21

--- cs-1400/hw_assignments/hw21.cpp
+++ cs-1400/hw_assignments/hw21.cpp
@@ -4,15 +4,45 @@
 
 int main()
 {
-    // 2. A parameter is a variable in the declaration of a function, while an argument is the actual data of that variable being passed into the function.
+    /*
+    2. A parameter is a variable in the declaration of a function, while an argument is the actual data of that variable being passed into the function.
 
-    // 3. Inside of the parentheses.
+    3. Inside of the parentheses.
 
-    // 5. Yes, the arguments get passed in order.
+    5. Yes, the arguments get passed in order.
 
-    // 7. It can divide a large problem into several smaller problems that are easily solved.
+    7. It can divide a large problem into several smaller problems that are easily solved.
 
-    // 10. function header
+    10. function header
 
-    // 11. void
+    11. void
+
+    12. definition, prototype
+
+    13. arguments
+
+    14. reference variables
+
+    15. reference
+
+    16. prototype function
+
+    32. showValue(12);
+
+    38. T
+
+    39. F
+
+    40. T
+
+    41. T
+
+    42. F
+
+    43. T
+
+    44. T
+    
+    45. F
+    */
 }
--- lab_files/lab08.cpp
+++ lab_files/lab08.cpp
@@ -1,3 +1,12 @@
 // author: Von Reid
 
 #include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    //
+}
