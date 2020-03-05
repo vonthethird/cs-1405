21c5b004165fefe866d050cd2f475127600d00d5
Von Reid <vontreid2000@gmail.com>
2020-03-05 00:28:56 -0700

    added comments cuz I'm a good boi

--- lab_files/lab08.cpp
+++ lab_files/lab08.cpp
@@ -8,7 +8,7 @@ using std::endl;
 using std::string;
 
 int turnsTaken, curPlayer, winPlayer;
-char userCell, userChar, a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i';
+char userCell, userChar, a, b, c, d, e, f, g, h, i;
 const int MAX_TURNS = 9;
 bool threeInARow;
 // displays the tic-tac-toe board depending on the letter(s) entered
@@ -21,11 +21,12 @@ bool hasWinner(bool);
 char getInput(char, char, char, char, char, char, char, char, char, char);
 
 // sub-level evaluations for getInput, just to separate it into smaller functions ;)
-void updateCell();
-void assignUserChar();
+void updateCell();     // replaces cell letters with Xs and Os
+void assignUserChar(); // determines whether the cell will have an X or an O based off of curPlayer number
 
 int main()
 {
+    a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i';
     cout << endl;
     cout << "----------------------------------" << endl;
     cout << "- Welcome to tic-tac-toe! -" << endl;
@@ -33,6 +34,7 @@ int main()
          << endl;
     cout << "Players will take turns choosing an unoccupied cell." << endl;
     cout << "The first player to get 3 in a row (or column or diagonal) wins!" << endl;
+
     for (turnsTaken = 0; turnsTaken < MAX_TURNS; turnsTaken++)
     {
         curPlayer = (turnsTaken % 2) + 1;
@@ -43,14 +45,18 @@ int main()
         hasWinner(threeInARow);
         if (threeInARow == true)
         {
+            // kills the loop, because you can end the game before turnsTaken == MAX_TURNS
             break;
         }
     }
-    displayBoard();
+
+    displayBoard(); // final board display
+
     if (threeInARow == true)
     {
         cout << "Player " << winPlayer << " won!" << endl;
     }
+    // only initiates if the users reach a stalemate
     else
     {
         cout << "Looks like a draw. Oof. Better start over bro." << endl;
@@ -70,8 +76,11 @@ void displayBoard()
 
 bool hasWinner(bool)
 {
+    // I know. These 2 bool variables are gross. But hey, it works lololol
     bool threeX = ((a == 'X' && d == 'X' && g == 'X') || (b == 'X' && e == 'X' && h == 'X') || (c == 'X' && f == 'X' && i == 'X') || (a == 'X' && b == 'X' && c == 'X') || (d == 'X' && e == 'X' && f == 'X') || (g == 'X' && h == 'X' && i == 'X') || (a == 'X' && e == 'X' && i == 'X') || (c == 'X' && e == 'X' && g == 'X'));
     bool threeO = ((a == 'O' && d == 'O' && g == 'O') || (b == 'O' && e == 'O' && h == 'O') || (c == 'O' && f == 'O' && i == 'O') || (a == 'O' && b == 'O' && c == 'O') || (d == 'O' && e == 'O' && f == 'O') || (g == 'O' && h == 'O' && i == 'O') || (a == 'O' && e == 'O' && i == 'O') || (c == 'O' && e == 'O' && g == 'O'));
+
+    // determines who the winner is after 3 cells in a row get filled
     if (threeX == true)
     {
         threeInARow = true;
@@ -99,6 +108,9 @@ char getInput(char, char, char, char, char, char, char, char, char, char)
 
 void updateCell()
 {
+    // because of how repetitive this switch is, my tingling spidey-senses tell me that there is most likely a better way to refactor this somehow, right?!
+    // unfortunately, I don't know if I'm fluent enough in C++ yet to figure it out yet. After all, I'm only halfway thru the semester yo. *shrug* I have to much hwk to do that I don't have time to dig into the possibilities.
+    // but I yeeted this out of getInput() into a separate function to make things look cleaner *dab*
     switch (userCell)
     {
     case 'a':
