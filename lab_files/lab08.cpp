8a0ca6b8ab7a687abea929f0d34c2c4713829d52
Von Reid <vontreid2000@gmail.com>
2020-03-04 23:41:41 -0700

    I GOT IT TO WORK OMG SO MUCH CHANGED CUZ IT WAS A MESS UGGGGHHH YES

--- lab_files/lab08.cpp
+++ lab_files/lab08.cpp
@@ -7,14 +7,15 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int turnsTaken, curPlayer;
+int turnsTaken, curPlayer, winPlayer;
 char userCell, userChar, a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i';
 const int MAX_TURNS = 9;
+bool threeInARow;
 // displays the tic-tac-toe board depending on the letter(s) entered
 void displayBoard();
 
 // determines who the winner is based which cells have been entered
-bool hasWinner(char, char, char, char, char, char, char, char, char);
+bool hasWinner(bool);
 
 // gathers the user inputs to select cell(s) on the board
 char getInput(char, char, char, char, char, char, char, char, char, char);
@@ -28,8 +29,21 @@ int main()
         cout << "Current Board:" << endl;
         displayBoard();
         getInput(curPlayer, a, b, c, d, e, f, g, h, i);
+        hasWinner(threeInARow);
+        if (threeInARow == true)
+        {
+            break;
+        }
     }
     displayBoard();
+    if (threeInARow == true)
+    {
+        cout << "Player " << winPlayer << " won!" << endl;
+    }
+    else
+    {
+        cout << "Looks like a draw. Oof. Better start over bro." << endl;
+    }
 }
 
 void displayBoard()
@@ -43,17 +57,26 @@ void displayBoard()
          << endl;
 }
 
-bool hasWinner(char, char, char, char, char, char, char, char, char)
+bool hasWinner(bool)
 {
-    bool threeInARow;
-    if ((a && d && g) == userChar || (b && e && h) == userChar || (c && f && i) == userChar || (a && b && c) == userChar || (d && e && f) == userChar || (g && h && i) == userChar || (a && e && i) == userChar || (c && e && g) == userChar)
+    bool threeX = ((a == 'X' && d == 'X' && g == 'X') || (b == 'X' && e == 'X' && h == 'X') || (c == 'X' && f == 'X' && i == 'X') || (a == 'X' && b == 'X' && c == 'X') || (d == 'X' && e == 'X' && f == 'X') || (g == 'X' && h == 'X' && i == 'X') || (a == 'X' && e == 'X' && i == 'X') || (c == 'X' && e == 'X' && g == 'X'));
+    bool threeO = ((a == 'O' && d == 'O' && g == 'O') || (b == 'O' && e == 'O' && h == 'O') || (c == 'O' && f == 'O' && i == 'O') || (a == 'O' && b == 'O' && c == 'O') || (d == 'O' && e == 'O' && f == 'O') || (g == 'O' && h == 'O' && i == 'O') || (a == 'O' && e == 'O' && i == 'O') || (c == 'O' && e == 'O' && g == 'O'));
+    //if ((a && d && g) == userChar || (b && e && h) == userChar || (c && f && i) == userChar || (a && b && c) == userChar || (d && e && f) == userChar || (g && h && i) == userChar || (a && e && i) == userChar || (c && e && g) == userChar)
+    if (threeX == true)
     {
         threeInARow = true;
+        winPlayer = 1;
+    }
+    else if (threeO == true)
+    {
+        threeInARow = true;
+        winPlayer = 2;
     }
     else
     {
         threeInARow = false;
     }
+    return threeInARow;
 }
 
 char getInput(char, char, char, char, char, char, char, char, char, char)
