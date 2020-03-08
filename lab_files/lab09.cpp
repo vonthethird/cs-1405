d51fe63337198466467952ad555e19128047c62f
Von Reid <vontreid2000@gmail.com>
2020-03-07 21:51:30 -0700

    almost got it workinggit add .!

--- lab_files/lab09.cpp
+++ lab_files/lab09.cpp
@@ -6,22 +6,24 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int primeList;
+bool checkIfPrime;
 
-int isPrime(const int, int);
+bool isPrime(int);
 
 int main()
 {
-    int userInput;
+    int inputQuant; // "Quant" meaning "Quantity," for the quantity of prime numbers
     cout << "Hello! Please enter how many prime numbers you would like to print out (starting from 1)." << endl;
-    cin >> userInput;
-    // I have to turn userInput into a const because arrays require const variables
-    const int inputQuant = userInput; // "Quant" meaning "Quantity," for the quantity of prime numbers
+    cin >> inputQuant;
     int primeList[inputQuant];
-    isPrime(inputQuant, primeList[inputQuant]);
-    for (int index = 0; index < inputQuant; index++)
+
+    for (int num = 1; num <= inputQuant; num++)
     {
-        cout << primeList << endl;
+        isPrime(num);
+        if (checkIfPrime == true)
+        {
+            cout << num << endl;
+        }
     }
     cout << endl
          << "Those are all of the first " << inputQuant << " prime numbers." << endl;
@@ -31,30 +33,20 @@ int main()
 
 // primeQuant is the same variable as inputQuant, just named differently for the different use in the functions.
 // i.e. main() uses it as user input, isPrime() uses it as the quantity of prime numbers to know how many of them to print out.
-int isPrime(const int primeQuant, int primeList[primeQuant])
+bool isPrime(int testNum)
 {
-    int num = 1;
-    bool checkIfPrime;
-
-    for (num; num < primeQuant; num++)
+    for (int i = 2; i <= testNum / 2; ++i)
     {
-        for (int i = 2; i <= num / 2; ++i)
+        if (testNum % i == 0)
         {
-            if (num % i == 0)
-            {
-                checkIfPrime = false;
-                break;
-            }
-            else // this else only exists because checkIfPrime doesn't have an initialized `true` value
-            {
-                checkIfPrime = true;
-            }
+            checkIfPrime = false;
+            break;
         }
-        if (checkIfPrime == true)
+        else // this else only exists because checkIfPrime doesn't have an initialized `true` value
         {
-            num >> primeList;
+            checkIfPrime = true;
         }
     }
 
-    return primeList;
+    return checkIfPrime;
 }
