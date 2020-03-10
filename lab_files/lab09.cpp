0da34ddb13319884ace24c723cb3b1f99d5c0c3c
Von Reid <vontreid2000@gmail.com>
2020-03-09 21:31:47 -0600

    more file setup

--- lab_files/lab09.cpp
+++ lab_files/lab09.cpp
@@ -8,7 +8,9 @@ using std::cin; // user input
 using std::cout; // message output
 using std::endl; // my professor recommends endl over \n but I can't remember why lol
 using std::string;
-using std::ofstream; // creates files
+using std::ofstream; // creates and writes to files
+using std::ifstream; // reads from files
+using std::fstream; // combo of ofstream and ifstream: creates, reads, and writes to files——but stack overflow peeps mostly recommend only using this for only both read and right, not one or the other
 
 bool checkIfPrime = false, testBoi = false;
 
