85de11d97af159038f2614c8e85ee2df2c2976f7
Von Reid <vontreid2000@gmail.com>
2020-03-12 08:34:32 -0600

    worksheet10 progress

--- lab_files/worksheet10.cpp
+++ lab_files/worksheet10.cpp
@@ -6,5 +6,17 @@ using namespace std; // just for now lol
 
 int main()
 {
-    // main() stuff
+    // 1. an array is allows you to store and work with multiple values of the same data type.
+
+    // 2. [0], [1], [2], and ]3]\
+
+    // 3.
+
+    // 4.
+
+    // 5. Write a C++ statement to define an array of 15 double precision floating-point numbers; name the variable "reals".
+    double reals[15];
+
+    // 6. Write a C++ statement to define a 10-element array of integers called ages initialized with the values 5, 7, 9, 14, 15, 17, 18, 19, 21, and 23.
+    int ages[10] = {5, 7, 9, 14, 15, 17, 18, 19, 21, 23};
 }
