d8846c905b917eaf00736f21b24a43ff3aac5e54
Von Reid <vontreid2000@gmail.com>
2020-03-17 19:06:15 -0600

    new stuff

--- extra_projects/arduino/motion_lights.ino
+++ extra_projects/arduino/motion_lights.ino
@@ -3,10 +3,31 @@
 
 void setUp(); // while no motion is detected
 
-//
+void motionDetected(); // do this when there IS motion detected
+
+void sleep(); // sleep after <x> amount of time door has remained shut
+
+void mainSwitch(); // loop through this for main functinonality
+
+int main()
+{
+    setUp(); // boot up the stuffs
+
+    return 0;
+}
 
 
 void setUp()
 {
     // setUp() stuff
+}
+
+void motionDetected()
+{
+    // motionDetected() stuff
+}
+
+void sleep()
+{
+    // sleep() stuff
 }
