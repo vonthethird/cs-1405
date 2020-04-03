2f269fd0e715a8770ff67addf5e2d3316e032230
Von Reid <vontreid2000@gmail.com>
2020-04-02 22:50:17 -0600

    worksheet11 completed

---
+++ lab_files/worksheet11.cpp
@@ -0,0 +1,23 @@
+// author: Von Reid
+
+/*
+
+1)
+1. is true, because words[0] is the exact same thing as words[0] (like if you did 5 == 5; it returns true because 5 is the exact same value as 5)
+3. is true, because the 0th index in the words vector does indeed come before the 1st index
+
+2)
+binaryFind is used with assert() statements to test vector index values.
+
+3)
+Display "What do you want to name file 1?".
+Input fileOneName.
+Display "What do you want to name file 2?".
+Input fileTwoName.
+File1:
+    Sort (valid) words alphabetically with their corresponding dictionary definitions.
+File2:
+    Test all of the words in this file to see if they exist in File1 (read into vector strings with binary searches).
+    Display the words that don't exist in File1.
+
+*/
