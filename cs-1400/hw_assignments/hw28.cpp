fda34915f45965f434bc2e7278071c0ffd188437
Von Reid <vontreid2000@gmail.com>
2020-04-03 23:20:20 -0600

    finished hw28

--- cs-1400/hw_assignments/hw28.cpp
+++ cs-1400/hw_assignments/hw28.cpp
@@ -1,2 +1,16 @@
 // author: Von Reid
 // this is for the H.W. #28 assignment in CS-1400
+
+#include <iostream>
+
+// 6. Because values move only one element at a time toward their final destination, so large arrays take forever to sort.
+
+// 7. Because the values move immediately to their final destination.
+
+// 12. ascending
+
+// 13. descending
+
+// 14. T
+
+// 15. F
