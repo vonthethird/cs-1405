7082f54aa17124cdc6d324f1e3ae381c8d644e17
Von Reid <vontreid2000@gmail.com>
2020-04-08 23:21:02 -0600

    not finished, cuz I suck

--- lab_files/lab11_project/lab11.cpp
+++ lab_files/lab11_project/lab11.cpp
@@ -1,3 +1,67 @@
 // author: Von Reid
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using std::string;
+using std::vector;
+
+// most important functions
+int binaryFind(const vector<string> &, const string &); // returns the index of str in the given sorted vector or -1 if not found
+
+// test functions
+void testDriver();
+
+int main()
+{
+    testDriver();
+
+    // main() shit
+}
+
+int binaryFind(const vector<string> &strs, const string &str)
+{
+    int index = 0;
+    string strValue;
+    do {
+        strValue = strs[index];
+        index++;
+    } while (strValue == string);
+    int maxVecSize = --index;
+    for (index = 0, index < maxVecSize; index++)
+    {
+        strValue = strs[index];
+        if (strValue == str)
+        {
+            return index;
+        }
+    }
+
+    return -1; // if the above for() loop can't find a match, it will return -1 to indicate that &str does not equal any of the values in the &strs vector
+}
+
+void testDriver()
+{
+    {
+        // test find empty
+        vector<string> strs;
+        assert(binaryFind(strs, "bye") == -1);
+    }
+    {
+        // test find single
+        vector<string> strs = {"bye"};
+        assert(binaryFind(strs, "bye") == 0);
+        assert(binaryFind(strs, "bo") == -1);
+    }
+    {
+        // test find multi
+        vector<string> strs = {"bye", "hello", "hi", "howdy"};
+        // linear find
+        assert(binaryFind(strs, "bye") == 0);
+        assert(binaryFind(strs, "where") == -1);
+        assert(binaryFind(strs, "hi") == 2);
+        assert(binaryFind(strs, "howdy") == 3);
+    }
+}
